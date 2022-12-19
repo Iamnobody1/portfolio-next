6463f43dcc10344ad1e5b712b3f9760c78c852a1
Iamnobody1 <Iamnobody1@users.noreply.github.com>
2022-12-19 14:27:20 +0000

    deploy: 41da6f3697f683890aa3fbdee1336bcb1284cc8f

---
+++ _next/static/chunks/pages/home/components/skills-3b9515af09e5218f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[611],{3024:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home/components/skills",function(){return u(6134)}])}},function(n){n.O(0,[134,774,888,179],function(){return n(n.s=3024)}),_N_E=n.O()}]);
